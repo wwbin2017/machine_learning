68d9cd6ff657ea829e29799db0c612237509f3c1
wwbin <wwbin2017@163.com>
2018-04-14 21:23:46 +0800

    add predcit module

--- topic_model/pLSA_EM.py
+++ topic_model/pLSA_EM.py
@@ -1,10 +1,12 @@
 #!/bin/python
 # -*- coding: UTF-8 -*-
 import random
+import json
+
 import numpy as np
 
 
-def count_WordDoc(doc_list):
+def count_WordDoc(doc_list, wordsid_file):
     """
     统计频率
     :param doc_list: 文档列表  [[word1 word2] [word1 word2]]
@@ -22,7 +24,38 @@ def count_WordDoc(doc_list):
             else:
                 doc_word[i][word] = doc_word[i][word] + 1
     words = list(words)
+    with open(wordsid_file, 'w') as fopen:
+        json.dump(dict(zip(words, range(len(words)))), fopen)
     return doc_word, words
+
+
+def load_word_id(wordsid_file):
+    with open(wordsid_file) as fopen:
+        word_id = json.load(fopen)
+    return word_id
+
+
+def save_model(doc_topic, topic_words):
+    """
+    
+    :param doc_topic: 
+    :param topic_words: 
+    :return: 
+    """
+    np.save("doc2topic.npy", doc_topic)
+    np.save("topic2words.npy", topic_words)
+
+
+def load_model(doc_topic_file="doc2topic.npy", topic_words_file="topic2words.npy"):
+    """
+    
+    :param doc_topic_file: 
+    :param topic_words_file: 
+    :return: 
+    """
+    doc_topic = np.load(doc_topic_file)
+    topic_words = np.load(topic_words_file)
+    return doc_topic, topic_words
 
 
 def plsa_em(doc_word, words, k, iter_num):
@@ -37,6 +70,7 @@ def plsa_em(doc_word, words, k, iter_num):
     for i in range(iter_num):
         e_step(doc_topic, topic_words, doc_k_words)
         m_step(doc_word_count, doc_topic, topic_words, doc_k_words)
+    save_model(doc_topic, topic_words)
     return doc_topic, topic_words
 
 
@@ -105,11 +139,34 @@ def init_param(doc_word, words, k):
     return doc_topic, topic_words, doc_k_words, doc_word_count
 
 
+def predict(topic_words, words, words_id):
+    w_id = list()
+    for w in words:
+        w_id.append(words_id[w] if w in words_id.keys() else 0)
+    dim = topic_words.shape[0]
+    docn_topic = np.zeros(dim)
+    for i in w_id:
+        docn_topic = docn_topic + topic_words[:, i]
+    docn_topic = docn_topic / sum(docn_topic)
+    return docn_topic
+
+
 if __name__ == "__main__":
-    doc_list = [[1,2,2,3,3], [2,2,3,3,3,4], [5,6,6,6,7,7,7,7], [66,6,6,6,7,6,7,7,7,8,9]]
-    doc_word, words = count_WordDoc(doc_list)
-    doc_topic, topic_words = plsa_em(doc_word, words, 2, 1000)
-    print doc_word
-    print words
-    print doc_topic
-    print topic_words
+    t = 1
+    if t == 0:
+        doc_list = [[1,2,2,3,3], [2,2,3,3,3,4], [5,6,6,6,7,7,7,7], [66,6,6,6,7,6,7,7,7,8,9]]
+        wordsid_file = "word_id"
+        doc_word, words = count_WordDoc(doc_list, wordsid_file)
+        doc_topic, topic_words = plsa_em(doc_word, words, 2, 20)
+
+    else:
+        # test
+        wordsid_file = "word_id"
+        doc_topic, topic_words = load_model()
+        words = [6,6,7,7,1,4]
+        words_id = load_word_id(wordsid_file)
+        print predict(topic_words, words, words_id)
+    # print doc_word
+    # print words
+    # print doc_topic
+    # print topic_words
