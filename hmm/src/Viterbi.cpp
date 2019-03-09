373bf60731a14fb364004ffd692f938a488e3701
wangwenbin <wangwenbin001@ke.com>
2019-03-09 14:21:18 +0800

    init

--- hmm/src/Viterbi.cpp
+++ hmm/src/Viterbi.cpp
@@ -20,69 +20,74 @@ class Viterbi{
         		infile >> _n >> _m;
 				_a.resize(_n);
 				_b.resize(_n);
+				std::cout << _n << " " << _m << std::endl;
 				for(int i = 0; i< _n; i+=1) {
-					std::vector<float> tmp_a(_n);
-					float t;
+					_a[i].resize(_n);
 					for(int j = 0; j < _n; j+=1) {
-						infile >> t;
-						tmp_a.push_back(t);
+						infile >> _a[i][j];
 					}
-					_a.push_back(tmp_a);
 				}
 				for(int i = 0; i< _n; i+=1) {
-					std::vector<float> tmp_a(_m);
-					float t;
+					_b[i].resize(_m);
 					for(int j = 0; j < _m; j += 1) {
-						infile >> t;
-						tmp_a.push_back(t);
+						infile >> _b[i][j];
 					}
-					_b.push_back(tmp_a);
 				}
-				float t;
+				_p.resize(_n);
 				for(int i = 0; i< _n; i+=1) {
-					infile >> t;
-					_p.push_back(t);
+					infile >> _p[i];
 				}
-
+				/**
+				for(int i = 0; i < _a.size();i++) {
+					for(int j = 0; j < _a[i].size(); j+=1){
+						std::cout << _a[i][j] << " ";
+					}
+					std::cout << std::endl;
+				}
+				for(int i = 0; i < _b.size();i++) {
+					for(int j = 0; j < _b[i].size(); j+=1){
+						std::cout << _b[i][j] << " ";
+					}
+					std::cout << std::endl;
+				}
+				for(int i = 0; i< _n; i++) {
+					std::cout << _p[i] << " ";
+				}
+				std::cout << std::endl;
+				**/
     		}
     		infile.close();
-			}
+		}
 
-			void predict(std::vector<int> o, float &max_p, std::vector<int> & path) {
+		void predict(std::vector<int> o, float &max_p, std::vector<int> & path) {
 			int T = o.size();
 			std::vector< std::vector<float> > delta(T);
 			std::vector< std::vector<int> > psi(T);
-			std::vector<float> delta_1(_n);
-			std::vector<int> psi_1(_n);
-			for(int i = 0; i < _n; i++) {
-				delta_1.push_back(_p[i]*_b[i][o[0]]);
-				psi_1.push_back(0);
+			for(int i = 0; i < T; i+=1){
+				delta[i].resize(_n);
+				psi[i].resize(_n);
 			}
-			delta.push_back(delta_1);
-			psi.push_back(psi_1);
+			for(int i = 0; i < _n; i++) {
+
+				delta[0][i] = _p[i]*_b[i][o[0]];
+				psi[0][i] = 0;
+			}
 			for(int t=1; t < T; t+=1) {
-				std::vector<float> delta_tmp(_n);
-				std::vector<int> psi_tmp(_n);
 				for(int i=0; i < _n; i+=1) {
-					float max_delta = 0.0;
+					float max_delta = -1;
 					int max_index_psi = 0;
-					float max_psi = 0.0;
 					for(int j = 0; j < _n; j+=1) {
 						float tmp = delta[t-1][j]*_a[j][i];
 						if (max_delta < tmp) {
 							max_delta = tmp;
-						}
-						if (tmp > max_index_psi) {
 							max_index_psi = j;
-							max_psi = tmp;
 						}
 					}
-					delta_tmp.push_back(max_delta*_b[i][o[t]]);
-					psi_tmp.push_back(max_index_psi);
+					delta[t][i] = max_delta*_b[i][o[t]];
+					psi[t][i] = max_index_psi;
 				}
-				delta.push_back(delta_tmp);
 			}
-			max_p = 0.0;
+			max_p = -1;
 			int max_index = 0;
 			for(int i = 0; i < _n; i+=1) {
 				if (delta[T-1][i] > max_p) {
--- hmm/src/test_viterbi.cpp
+++ hmm/src/test_viterbi.cpp
@@ -7,14 +7,14 @@ int main()
     string file_name = "../data/test";
     Viterbi viterbi_model(file_name);
 	int  v1[3] = {0,1,0};
-	vector<int> o(v1, &v1[2]);
+	vector<int> o(v1, &v1[3]);
 	float max_p = 0.0;
 	vector<int> path;
     viterbi_model.predict(o, max_p, path);
 	cout << "概率为：" << max_p <<endl;
 	cout << "预测值：" << " ";
 	for(int i = 0; i < path.size(); i+=1) {
-		cout <<path[i] << " ";
+		cout << path[i] << " ";
 	}
 	cout << endl;
     return 0;
